3f9d2e8899569e4d32e523e56ff3ded3716f9fc7
Sven Wappler <info@wappler.systems>
2024-02-13 22:02:58 +0100

    update

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -1,13 +1,11 @@
 <?php
 
-$newColumns = [
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', [
     'tags' => [
         'config' => [
             'type' => 'tag',
         ],
     ],
-];
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', $newColumns);
+]);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'tags', '', 'after:categories');
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,13 +1,11 @@
 <?php
 
-$newColumns = [
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', [
     'tags' => [
         'config' => [
             'type' => 'tag',
         ],
     ],
-];
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $newColumns);
+]);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'tags', '', 'after:categories');
