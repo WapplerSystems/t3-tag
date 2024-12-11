7068e906ffa63eff5d2cef781e9bc52bcd854128
Sven Wappler <wappler@wappler.systems>
2024-12-11 17:18:41 +0100

    update version compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF['tagging'] = [
     'title' => 'Tag field for elements like categories',
     'description' => 'A patch for TYPO3 to easily add tags just like categories to any element',
     'category' => 'fe',
-    'version' => '12.0.2',
+    'version' => '13.0.0',
     'state' => 'stable',
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'author_company' => 'WapplerSystems',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.4.99',
+            'typo3' => '13.0.0-13.4.99',
         ],
     ],
 ];
