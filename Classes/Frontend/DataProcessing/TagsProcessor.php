2e2aeb0d26504c5634926df40cb6ed2c1ab3afa6
Sven Wappler <wappler@wappler.systems>
2025-09-11 00:48:20 +0200

    updates

--- Classes/Frontend/DataProcessing/TagsProcessor.php
+++ Classes/Frontend/DataProcessing/TagsProcessor.php
@@ -2,6 +2,7 @@
 
 namespace TYPO3\CMS\Frontend\DataProcessing;
 
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
@@ -23,21 +24,36 @@ class TagsProcessor implements DataProcessorInterface
             return $processedData;
         }
 
-        if (
-            (isset($processorConfiguration['references']) && $processorConfiguration['references'])
-            || (isset($processorConfiguration['references.']) && $processorConfiguration['references.'])
-        ) {
-
-
-            if (!empty($processorConfiguration['references.'])) {
-                $referenceConfiguration = $processorConfiguration['references.'];
-                $relationField = $cObj->stdWrapValue('fieldName', $referenceConfiguration);
-
-            }
+        $fieldName = $cObj->stdWrapValue('fieldName', $processorConfiguration, 'tags');
+        if (($processedData['data'][$fieldName] ?? 0) === 0) {
+            return $processedData;
         }
 
+        $table = $cObj->stdWrapValue('table', $processorConfiguration, 'tt_content');
+        $uid = (int)$processedData['data']['uid'];
+
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable('sys_tag');
+
+        $tags = $queryBuilder
+            ->select('sys_tag.*')
+            ->from('sys_tag')
+            ->join(
+                'sys_tag',
+                'sys_tag_record_mm',
+                'mm',
+                $queryBuilder->expr()->eq('sys_tag.uid', $queryBuilder->quoteIdentifier('mm.uid_local'))
+            )
+            ->where(
+                $queryBuilder->expr()->eq('mm.tablenames', $queryBuilder->createNamedParameter($table)),
+                $queryBuilder->expr()->eq('mm.fieldname', $queryBuilder->createNamedParameter($fieldName)),
+                $queryBuilder->expr()->eq('mm.uid_foreign', $queryBuilder->createNamedParameter($uid))
+            )
+            ->executeQuery()
+            ->fetchAllAssociative();
+
         $targetVariableName = $cObj->stdWrapValue('as', $processorConfiguration, 'tags');
-        $processedData[$targetVariableName] = [];
+        $processedData[$targetVariableName] = $tags;
 
         return $processedData;
 
